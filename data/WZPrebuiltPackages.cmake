bfd60921fc06b02f42253af18d6c9f03e00f6c2c
DrBenson <Benson.Dr@GMail.com>
2024-06-25 13:36:48 +0800

    Update Terrain 2048x2048 texture high quality.

--- data/WZPrebuiltPackages.cmake
+++ data/WZPrebuiltPackages.cmake
@@ -4,3 +4,7 @@
 # terrain_overrides/high.wz
 set(WZ_PREBUILT_TERRAIN_HIGH_DL_URL "https://github.com/Warzone2100/data-terrain-high/releases/download/v1/high.wz")
 set(WZ_PREBUILT_TERRAIN_HIGH_DL_SHA512 "44a39e585344b25ab0fa4ed769f2b48bf23a0e4fc4b23e0fab4e68643cc6e3ebeabba790e8b1e6c96493ad01b6a2f09e92b893063013077dcac2475f0b5373d6")
+
+# terrain_overrides/high-hq.wz
+set(WZ_PREBUILT_TERRAIN_HIGH_HQ_DL_URL "https://github.com/DrBenson/warzone2100_data-terrain-high-hq/releases/download/v0/high-hq.wz")
+set(WZ_PREBUILT_TERRAIN_HIGH_HQ_DL_SHA512 "5055b0e69de4a610804eafc9e78aceda792f913deec5f1db95fe91703a47847e5a1531d05a94f8e6660ce5036b645a5260186245062fb749c1071b6aceede8c4")
